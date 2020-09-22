0c41bf44f12df29243a371dd3a4596d1915691de
dturnbu <60360077+dturnbu@users.noreply.github.com>
2020-09-22 15:20:25 -0500

    update

--- opa-server-cert.tf
+++ opa-server-cert.tf
@@ -1,12 +1,36 @@
-resource "tls_private_key" "opa_server_cert_ca_key" {
+#resource "tls_self_signed_cert" "opa_server_cert_ca" {
+#  is_ca_certificate     = true
+#  key_algorithm         = "RSA"
+#  private_key_pem       = tls_private_key.opa_server_cert_ca_key.private_key_pem
+#  validity_period_hours = 87600 # 10 years
+#  allowed_uses = [
+#    "digital_signature",
+#    "key_encipherment",
+#    "client_auth",
+#    "server_auth"
+#  ]
+#  subject {
+#    common_name         = "opa.opa.svc"
+#    organizational_unit = var.organizational_unit
+#    organization        = var.organization
+#    street_address      = var.street_address
+#    locality            = var.locality
+#    province            = var.province
+#    postal_code         = var.postal_code
+#    country             = var.country
+#    serial_number       = var.serial_number
+#  }
+#}
+#
+
+resource "tls_private_key" "opa_server_cert_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-resource "tls_self_signed_cert" "opa_server_cert_ca" {
-  is_ca_certificate     = true
+resource "tls_cert_request" "opa_server_cert_request" {
   key_algorithm         = "RSA"
-  private_key_pem       = tls_private_key.opa_server_cert_ca_key.private_key_pem
+  private_key_pem       = tls_private_key.opa_server_cert_key.private_key_pem
   validity_period_hours = 87600 # 10 years
   allowed_uses = [
     "digital_signature",
@@ -15,7 +39,7 @@ resource "tls_self_signed_cert" "opa_server_cert_ca" {
     "server_auth"
   ]
   subject {
-    common_name         = "opa.opa.svc"
+    common_name  = "${kubernetes_service.opa.metadata.0.name}.${kubernetes_namespace.opa_namespace.metadata.0.name}.svc"
     organizational_unit = var.organizational_unit
     organization        = var.organization
     street_address      = var.street_address
@@ -27,6 +51,17 @@ resource "tls_self_signed_cert" "opa_server_cert_ca" {
   }
 }
 
+resource "kubernetes_certificate_signing_request" "example" {
+  metadata {
+    name = "opa-server-cert-request"
+  }
+  auto_approve = true
+  spec {
+    usages = ["client auth", "server auth"]
+    request = tls_cert_request.opa_server_cert_request.cert_request_pem
+  }
+}
+
 resource "kubernetes_secret" "opa_server_cert" {
   metadata {
     name      = "opa-server-cert"
@@ -34,8 +69,8 @@ resource "kubernetes_secret" "opa_server_cert" {
   }
   type = "kubernetes.io/tls"
   data = {
-    "ca.crt" : tls_self_signed_cert.opa_server_cert_ca.cert_pem
-    "tls.crt" : tls_self_signed_cert.opa_server_cert_ca.cert_pem
-    "tls.key" : tls_self_signed_cert.opa_server_cert_ca.private_key_pem
+    #"ca.crt" : kubernetes_certificate_signing_request.example.certificate
+    "tls.crt" : kubernetes_certificate_signing_request.example.certificate
+    "tls.key" : tls_private_key.opa_server_cert_key.private_key_pem
   }
 }
