6e8940926393d3d308040ed220efe6a6a22708eb
dturnbu <60360077+dturnbu@users.noreply.github.com>
2020-09-23 12:01:52 -0500

    added depends on

--- opa-validating-webhook.tf
+++ opa-validating-webhook.tf
@@ -36,6 +36,7 @@
 # }
 
 resource "k8s_manifest" "app_projects" {
+  depends_on = [kubernetes_certificate_signing_request.opa_server_signed_cert]
   content = templatefile("${path.module}/templates/webhook-configuration.yaml", {
     cluster_ca_bundle : var.cluster_certificate_authority,
     service_name : kubernetes_namespace.opa_namespace.metadata.0.name,
