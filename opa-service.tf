26cd5702c5320e69c8c0cd7d849931e2b623acd9
dturnbu <60360077+dturnbu@users.noreply.github.com>
2020-09-23 09:26:43 -0500

    added a loadbalancer ingress to the opa service

--- opa-service.tf
+++ opa-service.tf
@@ -5,8 +5,13 @@ resource "kubernetes_service" "opa" {
     labels = merge({
       # Other labels go here
     }, local.resource_labels)
+    annotations = {
+      "service.beta.kubernetes.io/aws-load-balancer-internal": true
+      "service.beta.kubernetes.io/load-balancer-source-ranges": "10.0.0.0/8"
+    }
   }
   spec {
+    type = "LoadBalancer"
     selector = {
       "app" : "opa"
     }
