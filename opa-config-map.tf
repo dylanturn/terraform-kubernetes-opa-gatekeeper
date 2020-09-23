c26bbda921ae8ee4d34d93ee0968fb029a1abc28
dturnbu <60360077+dturnbu@users.noreply.github.com>
2020-09-23 14:17:08 -0500

    ignore openpolicyagent.org/policy-status annotations

--- opa-config-map.tf
+++ opa-config-map.tf
@@ -2,6 +2,7 @@ resource "kubernetes_config_map" "opa_default_system_main" {
   metadata {
     name      = "opa-default-system-main"
     namespace = kubernetes_namespace.opa_namespace.metadata.0.name
+    annotations = {}
     labels = merge({
       # Other labels go here
     }, local.resource_labels)
